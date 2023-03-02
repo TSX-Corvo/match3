7312d49f384535e0c511165bda8990e8d2f8034e
TSX-Corvo <cdbon15@gmail.com>
2023-03-02 12:40:50 -0400

    ...

--- src/states/PlayState.py
+++ src/states/PlayState.py
@@ -146,6 +146,16 @@ class PlayState(BaseState):
     def on_input(self, input_id: str, input_data: InputData) -> None:
         if not self.active:
             return
+        
+        if input_id == "right_click" and input_data.pressed:
+            pos_x, pos_y = input_data.position
+            pos_x = pos_x * settings.VIRTUAL_WIDTH // settings.WINDOW_WIDTH
+            pos_y = pos_y * settings.VIRTUAL_HEIGHT // settings.WINDOW_HEIGHT
+            i = (pos_y - self.board.y) // settings.TILE_SIZE
+            j = (pos_x - self.board.x) // settings.TILE_SIZE
+
+            if 0 <= i < settings.BOARD_HEIGHT and 0 <= j <= settings.BOARD_WIDTH:
+                self.board.tiles[i][j].color = 0
 
         if input_id == "click" and input_data.pressed:
             pos_x, pos_y = input_data.position
